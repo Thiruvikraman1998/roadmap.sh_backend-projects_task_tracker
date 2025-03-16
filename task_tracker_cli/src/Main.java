514c6344754b99cc8dde0286287907da825eca0f
thiruvikraman1998 <development.thiru@gmail.com>
2025-03-16 23:02:16 +0530

    initial commit

--- task_tracker_cli/src/Main.java
+++ task_tracker_cli/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Let's get started...");
+        System.out.println("Let's Build a Task Tracker command line tool!..");
     }
 }
